1d8167ffa5b8ddf559797e07b34dff27e97557a3
Mohammad Za'areer <zaa92reer@gmail.com>
2021-06-03 15:57:51 +0300

    lab-08 done

---
+++ src/error-handlers/404.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+
+function handle404(req, res, next) {
+
+  const errorObject = {
+    status: 404,
+    message: 'Not Found!'
+  }
+
+  res.status(404).json(errorObject);
+}
+
+module.exports = handle404;
